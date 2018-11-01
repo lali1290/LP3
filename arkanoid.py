39290fe88673f2611fe343da3be1404bb7d7f80d
TheRoniOne <42548202+TheRoniOne@users.noreply.github.com>
2018-11-01 17:18:00 -0500

    Update arkanoid.py

--- arkanoid.py
+++ arkanoid.py
@@ -22,7 +22,7 @@ class Bloque(pygame.sprite.Sprite):
 
 class Almacen(Bloque):
     def __init__(self):
-        self.puntaje = 29
+        self.puntaje = 0
     
     def generar(self,bloques): #generacion de bloques y coloreado inicial
         for j in range(3):
@@ -42,6 +42,8 @@ class Pelota(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self) #llamada al constructor de la clase padre
         self.image = pygame.Surface((25,25)) #creacion del sprite
         self.rect = self.image.get_rect() #se recrea su hitbox
+        
+    def posicionar(self):
         self.rect.x = 400-self.rect.width #posicionamiento en x
         self.rect.y = 600-self.rect.height-20 #posicionamiento en y
         
@@ -59,6 +61,8 @@ class Barra(pygame.sprite.Sprite):
         self.img = pygame.Surface((100,20)) #creacion del sprite
         self.img.fill((100,100,100)) #da color
         self.rect = self.img.get_rect() #se le da hitbox
+        
+    def posicionar(self):
         self.rect.x = 400-self.rect.width #posicionamiento en x
         self.rect.y = 600-self.rect.height #posicionamiento en y
     
@@ -82,74 +86,77 @@ def main():
     
     velocidad = [7,-7]
     
-    "vidas = 3"
+    vidas = 3
     
-    ejecutando = True
-    
-    while ejecutando:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                
-        pelota.mover(velocidad) #movimiento normal de la pelota
-        cont=1
-        colisiones = pygame.sprite.spritecollide(pelota, bloques, False) #devuelve una lista de colisiones entre la pelota y los bloques
+    while vidas >= 1:
+        ejecutando = True
+        pelota.posicionar()
+        barra.posicionar()
         
-        if (pelota.rect.y + pelota.rect.height >= 581): #logica de cambio de direccion si choca con barra
-            if (pelota.rect.colliderect(barra.rect)):
-                velocidad[1] = -1*velocidad[1]
-            else:
-                ejecutando = False #si no ha chocado con la barra es porque ya se le fue la bola y ha perdido
-                """vidas -= 1
-                if (vidas > 0):
-                    texto = "Le quedan " + vidas + ". Presione space para continuar"
-                    texto = fuente.render(texto, True, (0,0,0))
-                    pantalla.blit(texto, (400,300))
-                else:"""
-                print("Game Over") #convertilo en un mensaje visible en la pantalla
-        elif (len(colisiones) >= cont): #si la lista no esta vacia es porq la pelota ha chocado con un bloque
-            rnd = random.randint(0,1)
-            if (rnd ==0):
-                velocidad[0] = -1*velocidad[0]
-            velocidad[1] = -1*velocidad[1]
-            colisiones[0].colorear(almacen)
-            if (almacen.puntaje == 30):
-                """texto = fuente.render("You WIN!!!", False, (255,0,0))
-                pantalla.blit(texto, (400,300)) #arreglar """
-                print("You WIN!!!") #convertilo en un mensaje visible en la pantalla
-                #ejecutando = False
-        
-        if ((pelota.rect.x > (800 - pelota.rect.width)) or (pelota.rect.x < 0)):
-            velocidad[0] = -1*velocidad[0]
-        if (pelota.rect.y < 0):
-            velocidad[1] = -1*velocidad[1]
-    
-        keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_LEFT]: #si se presiona la tecla derecha o izquierda se mueve la barra
-            barra.mover(-10)
-        if keys[pygame.K_RIGHT]:
-            barra.mover(10)
-            
-        """if (vidas > 0) and (keys[pygame.K_SPACE]):
-            pelota.rect.x = 400-pelota.rect.width #devolver la pelota a su posicion original en x
-            pelota.rect.y = 600-pelota.rect.height-20 #devolver la pelota a su posicion original en y
-            barra.rect.x = 400-barra.rect.width #devolver la barra a su posicion original en x
-            barra.rect.y = 600-barra.rect.height #devolver la barra a su posicion original en y
-            ejecutando = True"""
-        
-        pantalla.fill((70, 242, 216))
-        
-        pantalla.blit(barra.img,barra.rect)
-        
-        pygame.draw.circle(pantalla,(100,100,100),(int(pelota.rect.x + pelota.rect.width/2), int(pelota.rect.y + pelota.rect.height/2)), 20)
-        
-        bloques.draw(pantalla)
+        while ejecutando:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
                     
-        pygame.display.flip()
+            pelota.mover(velocidad) #movimiento normal de la pelota
+            cont=1
+            colisiones = pygame.sprite.spritecollide(pelota, bloques, False) #devuelve una lista de colisiones entre la pelota y los bloques
+            
+            if (pelota.rect.y + pelota.rect.height >= 581): #logica de cambio de direccion si choca con barra
+                if (pelota.rect.colliderect(barra.rect)):
+                    velocidad[1] = -1*velocidad[1]
+                else:
+                    ejecutando = False #si no ha chocado con la barra es porque ya se le fue la bola y ha perdido
+                    vidas -= 1
+                    if (vidas > 0):
+                        texto = "Le quedan " + str(vidas) + ". Presione space para continuar"
+                        texto = fuente.render(texto, True, (0,0,0))
+                        pantalla.blit(texto, (400,300))
+                    else:
+                        print("Game Over") #convertilo en un mensaje visible en la pantalla
+            elif (len(colisiones) >= cont): #si la lista no esta vacia es porq la pelota ha chocado con un bloque
+                rnd = random.randint(0,1)
+                if (rnd ==0):
+                    velocidad[0] = -1*velocidad[0]
+                velocidad[1] = -1*velocidad[1]
+                colisiones[0].colorear(almacen)
+                if (almacen.puntaje == 30):
+                    """texto = fuente.render("You WIN!!!", False, (255,0,0))
+                    pantalla.blit(texto, (400,300)) #arreglar """
+                    print("You WIN!!!") #convertilo en un mensaje visible en la pantalla
+                    #ejecutando = False
+            
+            if ((pelota.rect.x > (800 - pelota.rect.width)) or (pelota.rect.x < 0)):
+                velocidad[0] = -1*velocidad[0]
+            if (pelota.rect.y < 0):
+                velocidad[1] = -1*velocidad[1]
         
-        reloj.tick(60)
+            keys = pygame.key.get_pressed()
+            
+            if keys[pygame.K_LEFT]: #si se presiona la tecla derecha o izquierda se mueve la barra
+                barra.mover(-10)
+            if keys[pygame.K_RIGHT]:
+                barra.mover(10)
+                
+            """if (vidas > 0) and (keys[pygame.K_SPACE]):
+                pelota.rect.x = 400-pelota.rect.width #devolver la pelota a su posicion original en x
+                pelota.rect.y = 600-pelota.rect.height-20 #devolver la pelota a su posicion original en y
+                barra.rect.x = 400-barra.rect.width #devolver la barra a su posicion original en x
+                barra.rect.y = 600-barra.rect.height #devolver la barra a su posicion original en y
+                ejecutando = True"""
+            
+            pantalla.fill((70, 242, 216))
+            
+            pantalla.blit(barra.img,barra.rect)
+            
+            pygame.draw.circle(pantalla,(100,100,100),(int(pelota.rect.x + pelota.rect.width/2), int(pelota.rect.y + pelota.rect.height/2)), 20)
+            
+            bloques.draw(pantalla)
+                        
+            pygame.display.flip()
+            
+            reloj.tick(60)
         
 if __name__ == "__main__":
     main()
